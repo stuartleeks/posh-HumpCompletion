378c775250fef8539b5899db27be18ae4620d85a
Stuart Leeks <stuart@leeks.net>
2016-05-10 23:46:32 +0200

    Rework to use AST, add initial parameter completion work

--- posh-HumpCompletion.ps1
+++ posh-HumpCompletion.ps1
@@ -1,8 +1,7 @@
 function DebugMessage($message) {
-    $threadId = [System.Threading.Thread]::CurrentThread.ManagedThreadId
-    $appDomainId = [AppDomain]::CurrentDomain.Id
-    [System.Diagnostics.Debug]::WriteLine("PoshHump: $threadId : $appDomainId :$message")
-
+    # $threadId = [System.Threading.Thread]::CurrentThread.ManagedThreadId
+    # $appDomainId = [AppDomain]::CurrentDomain.Id
+    # [System.Diagnostics.Debug]::WriteLine("PoshHump: $threadId : $appDomainId :$message")
     [System.Diagnostics.Debug]::WriteLine("PoshHump: $message")
 }
 
@@ -13,6 +12,7 @@ function GetCommandWithVerbAndHumpSuffix($commandName) {
         $suffix = $commandName.SubString($separatorIndex+1)
         return [PSCustomObject] @{
             "Verb" = $verb
+            "Suffix" = $suffix
             "SuffixHumpForm" = $suffix -creplace "[a-z]","" # case sensitive replace
             "Command" = $commandName 
         }   
@@ -82,20 +82,30 @@ function LoadHumpCompletionCommandCacheAsync(){
         $script:iar = $script:PowerShell.BeginInvoke()
     }
 }
-function PoshHumpTabExpansion($line) {
+function PoshHumpTabExpansion2($ast){
+    
+    $result = $null;
+    DebugMessage "In PoshHumpTabExpansion2"
+    $statements = $ast.EndBlock.Statements
+    $command = $statements.PipelineElements[$statements.PipelineElements.Count-1]
+    $commandName = $command.GetCommandName()
+    $commandElements = $command.CommandElements
 
-    if($line -match "^(?<verb>\S+)-(?<suffix>[A-Z]*)$") {
+    DebugMessage "$commandName"
+
+    if ( $commandElements.Count -eq 1) {         
+        ## if 1 command element then just the command (rather than parameters)
+        DebugMessage "single cmd element: $commandName"      
         EnsureHumpCompletionCommandCache
         
-        $command = $matches[0]
-        $commandInfo = GetCommandWithVerbAndHumpSuffix $command
-        $verb = $matches['verb']
-        $suffix= $matches['suffix']
+        $commandInfo = GetCommandWithVerbAndHumpSuffix $commandName
+        $verb = $commandInfo.Verb
+        $suffix= $commandInfo.Suffix
         $suffixWildcardForm = GetWildcardSuffixForm $suffix 
         $wildcardForm = "$verb-$suffixWildcardForm"
         $commands = $global:HumpCompletionCommandCache
         if ($commands[$verb] -ne $null) {
-            return $commands[$verb] `
+            $completionMatches = $commands[$verb] `
                 | Where-Object { 
                     # $_.Name is suffix hump form
                     # Match on hump form of completion word
@@ -105,8 +115,39 @@ function PoshHumpTabExpansion($line) {
                 | Select-Object -ExpandProperty Command `
                 | Where-Object { $_ -like $wildcardForm } `
                 | Sort-Object
+                
+            $result = [PSCustomObject]@{
+                ReplacementIndex = $command.Extent.StartOffset;
+                ReplacementLength = $command.Extent.EndOffset - $command.Extent.StartOffset;
+                CompletionMatches = $completionMatches
+            };
         }
+    } elseif ($commandElements.Count -gt 1 -and $commandElements[$commandElements.Count-1].GetType().Name -eq "CommandParameterAst"){
+        
+        $command = Get-Command  $commandName -ShowCommandInfo
+        if ($command.CommandType -eq "Alias") {
+            $command = Get-Command $command.Definition -ShowCommandInfo
+        }
+        
+        # complete the parameter!
+        $parameterElement = $commandElements[$commandElements.Count -1]
+        $parameterName = $parameterElement.ParameterName 
+        $wildcardForm = GetWildcardSuffixForm $parameterName
+        DebugMessage "multi cmd element. Parameter: '$parameterName', wildcardForm: $wildcardForm"      
+        # TODO - look at whether we can determine the parameter set to be smarter about the parameters we complete
+        $completionMatches = $command.ParameterSets `
+                                | Select-Object -ExpandProperty Parameters `
+                                | Select-Object -ExpandProperty Name -Unique `
+                                | Where-Object { $_ -clike $wildcardForm } `
+                                | Sort-Object
+
+        $result = [PSCustomObject]@{
+            ReplacementIndex = $parameterElement.Extent.StartOffset + 1; # +1 for the '-'
+            ReplacementLength = $parameterElement.Extent.EndOffset - $parameterElement.Extent.StartOffset - 1;
+            CompletionMatches = $completionMatches
+        };
     }
+    return $result
 }
 
 function Clear-HumpCompletionCommandCache() {
@@ -130,32 +171,93 @@ function Start-HumpCompletion(){
 }
 
 # install the handler!
-DebugMessage -message "Installing: Test PoshHumpTabExpansionBackup function"
+DebugMessage -message "Installing: Test PoshHumpTabExpansion2Backup function"
 if ($poshhumpSkipTabCompletionInstall){
     DebugMessage -message "Skipping tab expansion installation"
 } else {
-    if (-not (Test-Path Function:\PoshHumpTabExpansionBackup)) {
+    if (-not (Test-Path Function:\PoshHumpTabExpansion2Backup)) {
 
-        if (Test-Path Function:\TabExpansion) {
-            DebugMessage -message "Installing: Backup TabExpansion function"
-            Rename-Item Function:\TabExpansion PoshHumpTabExpansionBackup
+        if (Test-Path Function:\TabExpansion2) {
+            DebugMessage -message "Installing: Backup TabExpansion2 function"
+            Rename-Item Function:\TabExpansion2 PoshHumpTabExpansion2Backup
         }
+        
+        function TabExpansion2(){
+            <# Options include:
+                RelativeFilePaths - [bool]
+                    Always resolve file paths using Resolve-Path -Relative.
+                    The default is to use some heuristics to guess if relative or absolute is better.
 
-        function TabExpansion($line="", $lastWord="") {
-            $lastBlock = [regex]::Split($line, '[|;]')[-1].TrimStart()
+            To customize your own custom options, pass a hashtable to CompleteInput, e.g.
+                    return [System.Management.Automation.CommandCompletion]::CompleteInput($inputScript, $cursorColumn,
+                        @{ RelativeFilePaths=$false } 
+            #>
 
-            if ($global:HumpCompletionEnabled) {
-                DebugMessage -message "PoshHump:input: $lastBlock"
-                $result = PoshHumpTabExpansion $lastBlock
-            }
+            [CmdletBinding(DefaultParameterSetName = 'ScriptInputSet')]
+            Param(
+                [Parameter(ParameterSetName = 'ScriptInputSet', Mandatory = $true, Position = 0)]
+                [string] $inputScript,
+                
+                [Parameter(ParameterSetName = 'ScriptInputSet', Mandatory = $true, Position = 1)]
+                [int] $cursorColumn,
 
-            if ($result -ne $null) {
-                $result
-            } else {
-                # Fall back on existing tab expansion
-                if (Test-Path Function:\PoshHumpTabExpansionBackup) { 
-                    PoshHumpTabExpansionBackup $line $lastWord 
+                [Parameter(ParameterSetName = 'AstInputSet', Mandatory = $true, Position = 0)]
+                [System.Management.Automation.Language.Ast] $ast,
+
+                [Parameter(ParameterSetName = 'AstInputSet', Mandatory = $true, Position = 1)]
+                [System.Management.Automation.Language.Token[]] $tokens,
+
+                [Parameter(ParameterSetName = 'AstInputSet', Mandatory = $true, Position = 2)]
+                [System.Management.Automation.Language.IScriptPosition] $positionOfCursor,
+                
+                [Parameter(ParameterSetName = 'ScriptInputSet', Position = 2)]
+                [Parameter(ParameterSetName = 'AstInputSet', Position = 3)]
+                [Hashtable] $options = $null
+            )
+            End
+            {
+                if ($psCmdlet.ParameterSetName -eq 'ScriptInputSet')
+                {
+                    $results = [System.Management.Automation.CommandCompletion]::CompleteInput(
+                        <#inputScript#>  $inputScript,
+                        <#cursorColumn#> $cursorColumn,
+                        <#options#>      $options)
                 }
+                else
+                {
+                    $results = [System.Management.Automation.CommandCompletion]::CompleteInput(
+                        <#ast#>              $ast,
+                        <#tokens#>           $tokens,
+                        <#positionOfCursor#> $positionOfCursor,
+                        <#options#>          $options)
+                }
+                
+                if ($psCmdlet.ParameterSetName -eq 'ScriptInputSet')
+                {
+                    $ast = [System.Management.Automation.Language.Parser]::ParseInput($inputScript, [ref]$tokens, [ref]$null)
+                }
+                else
+                {
+                    $cursorColumn = $positionOfCursor.Offset
+                }
+
+                
+                $poshHumpResult = PoshHumpTabExpansion2 $ast
+                if ($poshHumpResult -ne $null){
+                    $results.ReplacementIndex = $poshHumpResult.ReplacementIndex
+                    $results.ReplacementLength = $poshHumpResult.ReplacementLength
+                    
+                    # From TabExpansionPlusPlus: Workaround where PowerShell returns a readonly collection that we need to add to.
+                    if ($results.CompletionMatches.IsReadOnly) {
+                        $collection = new-object System.Collections.ObjectModel.Collection[System.Management.Automation.CompletionResult]
+                        $results.GetType().GetProperty('CompletionMatches').SetValue($results, $collection)
+                    }
+                    
+                    $results.CompletionMatches.Clear() # TODO - look at inserting at front instead of clearing as this removes standard completion! Augment vs override
+                    $poshHumpResult.CompletionMatches | % { $results.CompletionMatches.Add($_)}
+                }
+                
+                return $results
             }
         }
         LoadHumpCompletionCommandCacheAsync
